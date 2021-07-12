fdf3720a39144bf227f4375d97b5e2c37f7b9904
lei hehl <lei.hehl@shopee.com>
2021-07-13 00:33:15 +0800

    fix: init vue 3.0

---
+++ Vue-Project/hello-vue3/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div v-for="item in list" :ref="setItemRef" :key="item">
+    <div>1</div>
+  </div>
+</template>
+
+<script>
+import { onBeforeUpdate, onUpdated } from "vue";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      list: [1,2,3,4]
+    }
+  },
+  setup() {
+    let itemRefs = [];
+
+    const setItemRef = el => {
+      itemRefs.push(el);
+    }
+
+    onBeforeUpdate(() => {
+      itemRefs = []
+    });
+
+    onUpdated(() => {
+      console.log(itemRefs)
+    });
+
+    return {
+      itemRefs,
+      setItemRef
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
